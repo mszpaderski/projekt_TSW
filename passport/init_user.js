7a88aac7da522ab0480a1c5e8c2c97f0f17aacad
Marcin Szpaderski <szpaderskimarcin@gmail.com>
2016-06-02 00:48:05 +0200

    merged and fixed passport initialization

---
+++ passport/init_user.js
@@ -0,0 +1,37 @@
+var login_admin = require('./login_admin');
+var signup_admin = require('./signup_admin');
+var login_judge = require('./login_judge');
+var signup_judge = require('./signup_judge');
+var Admin = require('../models/admin');
+var Judge = require('../models/judge');
+
+module.exports = function(passport){
+
+	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function(user, done) {
+        console.log('serializing user: ');console.log(user);
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        Admin.findById(id, function(err, user) {
+            if(err) done(err);
+             if(user){
+                console.log('deserializing user:',user);
+                done(err, user);
+             } else {
+                 Judge.findById(id, function(err, user){
+                     if(err) done(err);
+                     done(null, user);
+                 });
+             }
+        });
+    });
+
+    // Setting up Passport Strategies for Login and SignUp/Registration
+    login_admin(passport);
+    signup_admin(passport);
+    login_judge(passport);
+    signup_judge(passport);
+
+};
