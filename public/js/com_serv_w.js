d93961d6b73dc775e4ade8ec4d97aba579a0b44b
Your Name <mszpaderski@sigma.ug.edu.pl>
2016-06-07 12:41:22 +0200

    Added showing competitions for watchers

---
+++ public/js/com_serv_w.js
@@ -0,0 +1,14 @@
+//var socketIo = require('socket.io');
+//var socketIoClient = require('socket.io-client');
+
+
+window.addEventListener("load", function (event) {
+var socket = io.connect();
+
+//WATCHER SIDE
+        
+    
+    
+    
+    
+});
